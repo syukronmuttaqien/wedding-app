5cb73602d85c0a3c6efa2100c222cfcb359756a3
Syukron Muttaqien <syukronmuttaqien74@gmail.com>
2023-01-03 21:05:11 +0700

    Save progress

--- src/Components/Galery.tsx
+++ src/Components/Galery.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Fade } from 'react-awesome-reveal';
 import photo1 from '../images/photo1.jpg'
 import photo2 from '../images/photo2.jpg'
 import photo3 from '../images/photo3.jpg'
@@ -11,38 +12,53 @@ import BorderLine from './BorderLine';
 const Galery = () => {
     return (
         <div>
-            <h1 className='title-text'>Our Memories</h1>
-            <h3 className='fill-text quotes'>
-                &#8220; Berawal dari bercanda dan berakhir dengan rasa yang nyata &#8221;
-            </h3>
+            <Fade cascade direction='up'>
+                <h1 className='title-text'>Our Memories</h1>
+                <h3 className='fill-text quotes'>
+                    <Fade duration={200} cascade>&#8220; Berawal dari bercanda dan berakhir dengan rasa yang nyata &#8221;</Fade>
+                </h3>
+            </Fade>
             <div className='galery-container'>
-                <div>
-                    <div className='tape'></div>
-                    <img className='galery-image frame-image' src={photo1} />
-                </div>
+                <Fade direction='up'>
+                    <div>
+                        <div className='tape'></div>
+                        <img className='galery-image frame-image' src={photo1} />
+                    </div>
+                </Fade>
+                <Fade direction='up'>
                 <div>
                     <div className='tape'></div>
                     <img className='galery-image frame-image' src={photo2} />
                 </div>
+                </Fade>
+                <Fade direction='up'>
                 <div>
                     <div className='tape'></div>
                     <img className='galery-image frame-image' src={photo3} />
                 </div>
+                </Fade>
+
+                <Fade direction='up'>
                 <div>
                     <div className='tape'></div>
                     <img className='galery-image frame-image' src={photo4} />
                 </div>
+                </Fade>
+
+                <Fade direction='up'>
                 <div>
                     <div className='tape'></div>
                     <img className='galery-image frame-image' src={photo5} />
                 </div>
+                </Fade>
+
+                <Fade direction='up'>
                 <div>
                     <div className='tape'></div>
                     <img className='galery-image frame-image' src={photo6} />
                 </div>
+                </Fade>
             </div>   
-
-            
         </div>
     )
 }
--- src/Components/ThePromises.tsx
+++ src/Components/ThePromises.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import BorderLine from './BorderLine';
 
+import {Fade} from 'react-awesome-reveal';
+
 const ThePromises = () => {
     return (
         <div>
-            <h1 className='title-text'>The Promises</h1>
-            <p className='fill-text'>“Dan di antara tanda-tanda kekuasaan-Nya ialah Dia 
-                menciptakan untukmu pasangan-pasangan dari jenismu sendiri, 
-                supaya kamu cenderung dan merasa tenteram kepadanya,
-                dan dijadikan-Nya diantaramu rasa kasih dan sayang.
-                Sesungguhnya pada yang demikian itu benar-benar terdapat tanda-tanda bagi kaum yang berfikir.”
-            </p>
-            <h5 className='fill-text'> (QS Ar-Rum : 21) </h5>
-            <BorderLine />
+            <Fade triggerOnce cascade direction='up'>
+                <h1 className='title-text'>The Promises</h1>
+                <p className='fill-text'>“Dan di antara tanda-tanda kekuasaan-Nya ialah Dia 
+                    menciptakan untukmu pasangan-pasangan dari jenismu sendiri, 
+                    supaya kamu cenderung dan merasa tenteram kepadanya,
+                    dan dijadikan-Nya diantaramu rasa kasih dan sayang.
+                    Sesungguhnya pada yang demikian itu benar-benar terdapat tanda-tanda bagi kaum yang berfikir.”
+                </p>
+                <h5 className='fill-text'> (QS Ar-Rum : 21) </h5>
+                <BorderLine />
+            </Fade>
         </div>
     )
 }
