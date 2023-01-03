40055e8ac8c5c3b96cac7cf1283d3d40ac536cd6
Syukron Muttaqien <syukronmuttaqien74@gmail.com>
2023-01-03 23:32:00 +0700

    Add Music Stopper

--- src/Components/Message.tsx
+++ src/Components/Message.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
-import { Form } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Button, Form } from 'react-bootstrap'
 import {Fade} from 'react-awesome-reveal'
 import BorderLine from './BorderLine'
 
 const Message = () => {
+    const [messages, setMessages] = useState<Array<any>>([])
+    const [nama, setNama] = useState('')
+    const [message, setMessage] = useState('')
+
+    const sendMessage = () => {
+        const newMessages: Array<any> = messages.slice()
+
+        newMessages.unshift({
+            nama,
+            message
+        })
+
+        setMessages(newMessages)
+        setMessage('')
+        setNama('')
+    }
+
     return (
         <>  
             <Fade>
@@ -21,21 +38,23 @@ const Message = () => {
                 <Form className="message-container">
                     <Form.Group>
                         <h3 className='title-text'>Your Name</h3>
-                        <Form.Control type="text" placeholder="Please enter your name" />
+                        <Form.Control value={nama} onChange={(event) => setNama(event.target.value)} type="text" placeholder="Please enter your name" />
                     </Form.Group>
 
                     <Form.Group className='mt-4'>
                         <h3 className='title-text'>Your Message</h3>
-                        <Form.Control as="textarea" rows={3} placeholder="Your message here..." />
+                        <Form.Control value={message} onChange={(event) => setMessage(event.target.value)} as="textarea" rows={3} placeholder="Your message here..." />
                     </Form.Group>
                 </Form>
+                <button onClick={sendMessage} style={{width: 'fit-content'}} className='button-time'>Send</button>
                 <div className='mb-4'>
                     <div className='mt-4 message-list-container'>
-                        <div className='message-item-list'>
-                            <p className='item-name'>Syukron Yang Ganteng Tea</p>
-                            <p className='item-separator'>:</p>
-                            <p className='item-message'>Selamat Menempuh hidup baru, semoga langgeng sampe surga, aaaminnn</p>
-                        </div>
+                        {messages.map((r) => (
+                            <div className='message-item-list'>
+                                <span className='item-name'>{r.nama}</span>
+                                <span className='item-message'>{r.message}</span>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </Fade>
